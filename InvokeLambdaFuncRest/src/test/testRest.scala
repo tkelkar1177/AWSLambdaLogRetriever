42020ea875506951f4e01bda3c81b2793449fdd9
Tanmay Kelkar <tkelka2@uic.edu>
2021-11-18 19:58:26 -0600

    tests

---
+++ InvokeLambdaFuncRest/src/test/testRest.scala
@@ -0,0 +1,19 @@
+import LambdaInvocation.CallLambdaFunc.config
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class testgRPC extends AnyFlatSpec with Matchers {
+  behavior of "configuration parameters module"
+
+  it should "obtain the port value string" in {
+    config.getString("parameters.dt") shouldBe "00:00:01.000"
+  }
+
+  it should "obtain the url value string" in {
+    config.getString("parameters.url") shouldBe "https://nsefpc32vd.execute-api.us-east-1.amazonaws.com/default/TSAvailability/"
+  }
+
+  it should "obtain the output file path string" in {
+    config.getString("parameters.fileOutputPath") shouldBe "OutputLogs/MatchedLogs.txt"
+  }
+}
